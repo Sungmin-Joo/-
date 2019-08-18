8d0074eb856051c49c8abdcbf20c7b3f34099881
Sung-Min Joo <big-joo_dev@naver.com>
2019-08-18 23:59:50 +0900

    Create papago_test.py

---
+++ example/papago_test.py
@@ -0,0 +1,17 @@
+﻿# -*- coding: utf-8 -*-
+import requests
+
+header = {
+    'X-Naver-Client-Id' : '--------------------',
+    'X-Naver-Client-Secret' : '----------'
+}
+
+datas = {
+    "source" : "ko",
+    "target" : "en",
+    "text" : "딸기"
+}
+url = 'https://openapi.naver.com/v1/language/translate'
+
+response=requests.post(url = url, headers=header, data=datas).json()
+print(response['message']['result']['translatedText'])
